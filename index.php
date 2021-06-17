cdab5c09244575cd81bc15d8ff237ac8309c2d3d
WilbertRayos <wilbertraymundrayos@gmail.com>
2021-06-17 22:31:38 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/top_header.css" />
+</head>
+<body>
+    <div id="main_wrapper">
+        <header id="top_header">
+            This is the header
+        </header>
+        <nav id="menubar">
+            <li>Home</li>
+            <li>Certificates</li>
+            <li>Downloads</li>
+            <li>Contacts</li>
+        </nav>
+        <section id="main_section">
+            <article>
+                <header>
+                   <hgroup>
+                       <h2>This is the article subtitle</h2>
+                       <h3>wewews</h3>
+                   </hgroup>
+                </header>
+                <p>Main article</p>
+                <footer>
+                    Article footer
+                </footer>
+            </article>
+        </section>
+        <aside id="side_section">
+            This is sidebar
+        </aside>
+        <footer id="footer">
+            This is footer
+        </footer>
+    </div>
+</body>
+</html>
